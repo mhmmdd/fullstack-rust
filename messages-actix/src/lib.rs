1581fb7f4fa226d2916298ced273b3db2a69de89
mhmmdd <mhmmd.dd@gmail.com>
2021-01-09 00:16:59 +0300

    add AppState

--- messages-actix/src/lib.rs
+++ messages-actix/src/lib.rs
@@ -1,8 +1,14 @@
 #[macro_use]
 extern crate actix_web;
 
-use actix_web::{App, HttpRequest, HttpServer, middleware, Result, web};
+use std::cell::Cell;
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use actix_web::{App, HttpServer, middleware, Result, web};
 use serde::Serialize;
+
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 pub struct MessageApp {
     port: u16,
@@ -10,22 +16,29 @@ pub struct MessageApp {
 
 #[derive(Serialize)]
 struct IndexResponse {
-    message: String
+    server_id: usize,
+    request_count: usize,
+    message: Vec<String>,
+}
+
+struct AppState {
+    server_id: usize,
+    request_count: Cell<usize>,
+    messages: Arc<Mutex<Vec<String>>>,
 }
 
 #[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
-    let hello = req
-    .headers()
-    .get("hello")
-    .and_then(|v| v.to_str().ok())
-    .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let ms = state.messages.lock().unwrap();
 
     Ok(web::Json(IndexResponse {
-        message: hello.to_owned(),
+        server_id: state.server_id,
+        request_count,
+        message: ms.to_owned(),
     }))
 }
-
 
 impl MessageApp {
     pub fn new(port: u16) -> Self {
@@ -34,8 +47,15 @@ impl MessageApp {
 
     pub fn run(&self) -> std::io::Result<()> {
         println!("Starting http server 127.0.0.1:{}", self.port);
+
+        let messages = Arc::new(Mutex::new(vec![]));
         HttpServer::new(move || {
             App::new()
+                .data(AppState {
+                    server_id : SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
+                    request_count: Cell::new(0),
+                    messages: messages.clone()
+                })
                 .wrap(middleware::Logger::default())
                 .service(index)
         })
