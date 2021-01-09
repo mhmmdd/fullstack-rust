97a9de2d25ba3e5eebd25b8d1292d012cbf0359d
mhmmdd <mhmmd.dd@gmail.com>
2021-01-09 20:05:36 +0300

    receiving input

---
+++ messages-actix/README.md
@@ -0,0 +1,18 @@
+## Example Requests
+`$ curl localhost:8082`
+_{"server_id":0,"request_count":9,"message":[]}_
+
+`$ curl -X POST -H "Content-Type: application/json" -d '{"message": "helllo"}' localhost:8082/send`
+_{"server_id":1,"request_count":1,"message":"hello"}_
+
+`$ curl -X POST -H "Content-Type: application/json" -d '{"message": "helllo2"}' localhost:8082/send`
+_{"server_id":1,"request_count":1,"message":"hello2"}_
+
+`$ curl localhost:8082`
+_{"server_id":1,"request_count":3,"message":["helllo","helllo2"]}_
+
+`$ curl -X POST localhost:8082/clear`
+_{"server_id":4,"request_count":1,"message":[]}_
+
+`$ curl localhost:8082`
+_{"server_id":1,"request_count":3,"message":[]}_
--- messages-actix/src/lib.rs
+++ messages-actix/src/lib.rs
@@ -6,7 +6,7 @@ use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use actix_web::{App, HttpServer, middleware, Result, web};
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
 static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
@@ -27,6 +27,18 @@ struct AppState {
     messages: Arc<Mutex<Vec<String>>>,
 }
 
+#[derive(Deserialize)]
+struct PostInput {
+    message: String
+}
+
+#[derive(Serialize)]
+struct PostResponse {
+    server_id: usize,
+    request_count: usize,
+    message: String,
+}
+
 #[get("/")]
 fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
     let request_count = state.request_count.get() + 1;
@@ -40,6 +52,35 @@ fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
     }))
 }
 
+#[post("/clear")]
+fn clear(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.clear();
+
+    Ok(web::Json(IndexResponse {
+        server_id: state.server_id,
+        request_count,
+        message: vec![],
+    }))
+}
+
+
+fn post(msg: web::Json<PostInput>, state: web::Data<AppState>) -> Result<web::Json<PostResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.push(msg.message.clone());
+
+    Ok(web::Json(PostResponse {
+        server_id: state.server_id,
+        request_count,
+        message: msg.message.clone(),
+    }))
+}
+
+
 impl MessageApp {
     pub fn new(port: u16) -> Self {
         MessageApp { port }
@@ -52,15 +93,20 @@ impl MessageApp {
         HttpServer::new(move || {
             App::new()
                 .data(AppState {
-                    server_id : SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
+                    server_id: SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
                     request_count: Cell::new(0),
-                    messages: messages.clone()
+                    messages: messages.clone(),
                 })
                 .wrap(middleware::Logger::default())
                 .service(index)
+                .service(web::resource("/send")
+                    .data(web::JsonConfig::default().limit(4096))
+                    .route(web::post().to(post))
+                )
+                .service(clear)
         })
-        .bind(("127.0.0.1", self.port))?
-        .workers(8)
-        .run()
+            .bind(("127.0.0.1", self.port))?
+            .workers(8)
+            .run()
     }
 }
